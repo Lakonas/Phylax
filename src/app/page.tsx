3075075019159deb0458690bb4b4910aedca1ab8
Bill Katsoulis <bill.katsoulis@rogers.com>
2026-03-13 20:18:04 -0400

    feat: redirect home page based on auth status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1>Phylax</h1>;
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+
+export default async function Home() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('token');
+
+  if (token) {
+    redirect('/queue');
+  } else {
+    redirect('/login');
+  }
 }
